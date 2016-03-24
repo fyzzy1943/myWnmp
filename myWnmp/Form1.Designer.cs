86908e6bd281ff8769dfb2e05f6300ce3a6a742d
fyzzy1943 <fyzzy1943@gmail.com>
2016-03-24 22:44:20 +0800

    faster

---
+++ myWnmp/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace myWnmp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "myWnmp";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 48);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(49, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 147);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "myWnmp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ myWnmp/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myWnmp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("start");
+            if (Process.GetProcessesByName("nginx").Length > 0
+                || Process.GetProcessesByName("php-cgi").Length > 0)
+            {
+                return;
+            }
+
+            ExecBatCommand(p =>
+            {
+                p(@"D:\web\nginx-1.9.12\RunHiddenConsole.exe D:\web\php-7.0.4-nts-Win32-VC14-x64\php-cgi.exe -b 127.0.0.1:9000-c D:\web\php-7.0.4-nts-Win32-VC14-x64\php.ini");
+                p(@"D:\web\nginx-1.9.12\RunHiddenConsole.exe D:\web\nginx-1.9.12\nginx.exe -p D:\web\nginx-1.9.12");
+                p("exit 0");
+            });
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExecBatCommand(p =>
+            {
+                p(@"taskkill /F /IM nginx.exe > nul");
+                p(@"taskkill /F /IM php-cgi.exe > nul");
+                p("exit 0");
+            });
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int nginx = 0, php = 0, mysql = 0;
+            string status = "";
+
+            while (true)
+            {
+                status = "";
+
+                nginx = Process.GetProcessesByName("nginx").Length;
+                status += "nginx is " + (nginx > 0 ? "running" : "stop")
+                    + ", " + nginx.ToString() + "process.\n";
+
+                php = Process.GetProcessesByName("php-cgi").Length;
+                status += "php is " + (php > 0 ? "running" : "stop")
+                    + ", " + php.ToString() + "process.\n";
+
+                mysql = Process.GetProcessesByName("mysqld").Length;
+                status += "mysql is " + (mysql > 0 ? "running" : "stop")
+                    + ", " + mysql.ToString() + "process.\n";
+
+                CheckStatus(status);
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+        private delegate void CheckStatusDelegate(string content);
+
+        private void CheckStatus(string content)
+        {
+            if (label1.InvokeRequired)
+            {
+                label1.Invoke(new CheckStatusDelegate(CheckStatus), content);
+            }
+            else
+            {
+                label1.Text = content;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        /// <summary>
+        /// 打开控制台执行拼接完成的批处理命令字符串
+        /// </summary>
+        /// <param name="inputAction">需要执行的命令委托方法：每次调用 <paramref name="inputAction"/> 中的参数都会执行一次</param>
+        private static void ExecBatCommand(Action<Action<string>> inputAction)
+        {
+            Process pro = null;
+            StreamWriter sIn = null;
+            StreamReader sOut = null;
+
+            try
+            {
+                pro = new Process();
+                pro.StartInfo.FileName = "cmd.exe";
+                pro.StartInfo.UseShellExecute = false;
+                pro.StartInfo.CreateNoWindow = true;
+                pro.StartInfo.RedirectStandardInput = true;
+                pro.StartInfo.RedirectStandardOutput = true;
+                pro.StartInfo.RedirectStandardError = true;
+
+                pro.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+                pro.ErrorDataReceived += (sender, e) => Console.WriteLine(e.Data);
+
+                pro.Start();
+                sIn = pro.StandardInput;
+                sIn.AutoFlush = true;
+
+                pro.BeginOutputReadLine();
+                inputAction(value => sIn.WriteLine(value));
+
+                pro.WaitForExit();
+            }
+            finally
+            {
+                if (pro != null && !pro.HasExited)
+                    pro.Kill();
+
+                if (sIn != null)
+                    sIn.Close();
+                if (sOut != null)
+                    sOut.Close();
+                if (pro != null)
+                    pro.Close();
+            }
+        }
+    }
+}
